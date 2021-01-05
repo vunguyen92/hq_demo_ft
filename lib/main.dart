8d19e2a05555c448e9753b877bd56c98f9f2ca7e
vunh <huuvu1992@gmail.com>
2021-01-05 22:41:48 +0700

    thêm view BottomBar

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,98 @@
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-//    final wordPair = WordPair.random();
     return MaterialApp(
       home: RandomWords(),
-      /*home: Container(
-        color: Colors.white,
-      ),*/
     );
   }
 }
+
+class HomePage extends StatefulWidget {
+  HomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  Color colorPrimary = Colors.blueAccent;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: new Container(),
+      bottomNavigationBar: _bottomBar(),
+    );
+  }
+
+  Widget _bottomBar(){
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: bottomItemsList,
+      currentIndex: _selectedIndexBottomBar,
+      selectedItemColor: colorPrimary,
+      onTap: _onItemTapped,
+    );
+  }
+  int _selectedIndexBottomBar = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndexBottomBar = index;
+      Fluttertoast.showToast(
+        msg: "index = " + index.toString(),
+        toastLength: Toast.LENGTH_SHORT,
+        gravity: ToastGravity.BOTTOM,
+      );
+    });
+  }
+
+  List<BottomNavigationBarItem> bottomItemsList = [
+    BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/images/ic_bot_homepage.png"),
+          color: Colors.grey),
+      activeIcon:
+      ImageIcon(AssetImage("assets/images/ic_bot_homepage_focus.png")),
+      title: Text('Home'),
+    ),
+    BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/images/ic_bot_category.png"),
+          color: Colors.grey),
+      activeIcon:
+      ImageIcon(AssetImage("assets/images/ic_bot_category_focus.png")),
+      title: Text('Shop'),
+    ),
+    BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/images/ic_bot_messenger.png"),
+          color: Colors.grey),
+      activeIcon:
+      ImageIcon(AssetImage("assets/images/ic_bot_messenger_focus.png")),
+      title: Text('Chat'),
+    ),
+    BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/images/ic_bot_shop.png"),
+          color: Colors.grey),
+      activeIcon: ImageIcon(AssetImage("assets/images/ic_bot_shop_focus.png")),
+      title: Text('My Orders'),
+    ),
+    BottomNavigationBarItem(
+      icon: ImageIcon(AssetImage("assets/images/ic_bot_account.png"),
+          color: Colors.grey),
+      activeIcon:
+      ImageIcon(AssetImage("assets/images/ic_bot_account_focus.png")),
+      title: Text('Account'),
+    ),
+  ];
+}
+
 class RandomWords extends StatefulWidget {
   @override
   RandomWordsState createState() => new RandomWordsState();
